16905d41959776ec2d6f05079c0a82991ab6c4ef
Evgeniy <evglaptev23@gmail.com>
2017-07-17 21:20:32 +0300

    add infinity steps

--- src/Game.js
+++ src/Game.js
@@ -1,7 +1,7 @@
 import Rx from 'rxjs/Rx'
 const HEAD_CODE = 1;
 class Game {
-  constructor(head_ = {x:0,y:0},vector_={x:1,y:0}, size_ = 10, tick_ = 300) {
+  constructor(head_ = {x:0,y:0},vector_={x:1,y:0}, size_ = 20, tick_ = 30) {
     this.head = head_;
     this.size = size_;
     this.tick = tick_;
@@ -22,23 +22,20 @@ class Game {
         }
       }
       return arr;
-  }
+  };
 
   dataAsObservable = () => {
     return Rx.Observable.create((observer) => {
-      setInterval(() => observer.next(this.data), this.tick)
+      setInterval(() => observer.next(this.data), this.tick*2)
     })
-  }
+  };
 
   setVector = (vector_) =>{ // 0,1; 0,-1; 1,0; -1,0;
     switch (vector_.key) {
       case 'ArrowUp':
-        console.log(this);
         this.vector = {x:0,y:-1};
-        console.log(this.vector);
         break;
       case 'ArrowRight':
-      console.log("{x:1,y:0}");
         this.vector = {x:1,y:0};
         break;
       case 'ArrowDown':
@@ -49,22 +46,25 @@ class Game {
         break;
 
     }
-  }
+  };
 
   start(){
     setInterval(this.gameTick, this.tick)
   }
 
   gameTick = () => {
-    console.log(this.vector.x);
     let nextHead = {
       x: this.head.x + this.vector.x,
       y: this.head.y + this.vector.y
-    }
-    this.head = nextHead
+    };
+      nextHead.x = nextHead.x <0? this.size + nextHead.x : nextHead.x
+      nextHead.y = nextHead.y <0? this.size + nextHead.y : nextHead.y
+      nextHead.x = Math.round((nextHead.x>=this.size)?nextHead.x/this.size-1:nextHead.x);
+      nextHead.y = Math.round((nextHead.y>=this.size)?nextHead.y/this.size-1:nextHead.y);
+      console.log(nextHead)
+    this.head = nextHead;
     this.data = this.data.map((value,key) => {
       if (key === this.getIndex(nextHead)){
-        console.log(nextHead.x);
         return HEAD_CODE;
       } else if (key === this.getIndex(this.tail)) {
         return 0;
@@ -72,12 +72,8 @@ class Game {
         value;
       }
     })
-  }
-
-
+  };
   getIndex = (cell) => cell.y*this.size+cell.x
-
-
 }
 
 export default Game;
